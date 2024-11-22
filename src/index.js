58faad71a24dcc19e2ece89979301dc8c33607bf
MichelleMOU86 <Michelle.L.Munoz@gmail.com>
2024-11-21 22:36:15 -0500

    Created HTML and CSS for forecast, created JS function

--- src/index.js
+++ src/index.js
@@ -50,9 +50,29 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  let forecastHtml = "";
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+    <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-icon">☀️</div>
+    <div class="weather-forecast-temp">
+      <div class="weather-forecast-temps"><strong>15°F</strong></div>
+      <div class="weather-forecast-temps">9°F</div>
+    </div>
+  </div>`;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 
 let searchFormElement = document.querySelector("#search-form");
 
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Athens");
+
+displayForecast();
